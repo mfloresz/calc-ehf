71fa77493396808c82043c6f6d8b469e12343a7f
mfloresz <31319887+mfloresz@users.noreply.github.com>
2019-10-09 16:53:34 -0500

    Add files via upload

--- assets/js/code.js
+++ assets/js/code.js
@@ -2,7 +2,8 @@ function CalcImp() {
     var Num1 = Number(document.getElementById('PedidoV').value)
     var Num2 = Number(document.getElementById('PrecioC').value)
     var Num3 = Number(document.getElementById('CostoM').value)
-    var Res = Num1 * Num2;
+    var Res1 = (Num1 * Num2);
+    var Res = Math.round(Res1);
     document.getElementById('ImpTot').value = Res;
     return false
 }
