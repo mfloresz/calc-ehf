c9ca9431511d9fc6afe6e37bef87d657a2f53863
mfloresz <31319887+mfloresz@users.noreply.github.com>
2021-01-09 16:05:00 -0600

    Update code.js

--- assets/js/code.js
+++ assets/js/code.js
@@ -61,7 +61,7 @@ function EstKg() {
     var Num6 = Number(document.getElementById('LongVara').value)
     var Num7 = Number(document.getElementById('PzaSol').value)
     var EstimKg = ((Num6 * 2.515) * Num7) / 1000;
-    var EstimKg = EstimKg + (EstimKg * 0.07);
+    var EstimKg = EstimKg + (EstimKg * 0.09);
     document.getElementById('KgEst').value = EstimKg;
     return false
 }
